aaf98bba895c1f7c597b113c65ea1c8b4d1adefe
mathius44 <nicolasmathius80@gmail.com>
2025-07-14 12:03:11 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+// script.js
+const cursos = {
+  mate1: "Matemática I: Cálculo diferencial. Prerrequisito: ninguno.",
+  fis1: "Física I: Cinemática. Prerrequisito: Matemática I."
+};
+
+function mostrarInfo(codigo) {
+  const modal = document.getElementById("info-curso");
+  modal.innerHTML = cursos[codigo];
+  modal.classList.remove("oculto");
+}
 
